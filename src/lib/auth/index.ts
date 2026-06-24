39bdf61cc799da8a777e4f84683afc52b3955bd7
Pavel <azazasdfg.com@gmail.com>
2026-06-25 02:30:55 +0300

    config: Add host to trusted hosts

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -10,6 +10,7 @@ import {
 } from "@/lib/database/schema"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  trustHost: true,
   adapter: DrizzleAdapter(db, {
     usersTable: users,
     accountsTable: accounts,
