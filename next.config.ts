53e6189dd8f2b23bfbe418bfce5b33fc2f9e7078
Pavel <azazasdfg.com@gmail.com>
2026-06-26 03:36:37 +0300

    config: Move Dockerfile

--- next.config.ts
+++ next.config.ts
@@ -6,12 +6,13 @@ const nextConfig: NextConfig = {
   cacheComponents: true,
   images: {
     qualities: [75, 100],
+    // remotePatterns have no effect in production
     remotePatterns: [
       {
         protocol: 'http',
         hostname: 'localhost',
-        port: '3000', // e.g., '3000', '8000', or '1337'
-        pathname: '/api/images/*', // e.g., '/media/**', or '/**' for any path
+        port: '3000',
+        pathname: '/api/images/*',
       },
     ]
   }
