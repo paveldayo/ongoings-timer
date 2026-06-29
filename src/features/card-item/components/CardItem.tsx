b5966a7a6678e1fad4c9d424f9d7f7c242d1552e
Pavel <azazasdfg.com@gmail.com>
2026-06-30 01:46:38 +0300

    feat: Minimize card if show watched

--- src/features/card-item/components/CardItem.tsx
+++ src/features/card-item/components/CardItem.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import { Button } from "@/components/ui/button"
 import { Card } from "@/entities/card/types"
 import { MinusIcon, MoreVerticalIcon, PlusIcon } from "lucide-react"
@@ -8,7 +9,8 @@ import { useLayoutEffect, useState } from "react"
 import { toast } from "sonner"
 import CardItemActions from "./CardItemActions"
 import { changeWatched } from "../actions/changeWatched"
-import { formatCountdown } from "../utils"
+import { formatCountdown, isWatched } from "../utils"
+import { cn } from "@/utils/shadcn/utils"
 
 interface Props {
   card: Card
@@ -17,14 +19,15 @@ export default function CardItem({ card }: Props) {
   const router = useRouter()
   const [countdown, setCountdown] = useState<string | null>(null)
 
+  // Start countdown
   useLayoutEffect(() => {
-    const update = () => {
+    const updateTimer = () => {
       setCountdown(formatCountdown(new Date(card.next_episode_at)))
     }
-    update()
-    
-    const countdownIntervalId = setInterval(update, 1000)
-    
+    updateTimer()
+
+    const countdownIntervalId = setInterval(updateTimer, 1000)
+
     return () => {
       clearInterval(countdownIntervalId)
     }
@@ -42,7 +45,10 @@ export default function CardItem({ card }: Props) {
   }
 
   return (
-    <div className="h-60 border rounded-md overflow-hidden shadow-[-3px_4px_10px_0_rgba(0,0,0,0.25)] flex">
+    <div className={cn(
+      "h-60 border rounded-md overflow-hidden shadow-[-3px_4px_10px_0_rgba(0,0,0,0.25)] flex transition-opacity hover:opacity-100",
+      { "opacity-80 h-25": isWatched(card) },
+    )}>
       <Image
         src={`/api/images/${card.image_key}`}
         alt="Ado-san :з"
@@ -50,23 +56,27 @@ export default function CardItem({ card }: Props) {
         height={580}
         sizes="100vw"
         // sizes="(max-width: 768px) 100vw, 600px"
-        className="object-cover aspect-5/7 w-auto h-full"
+        className={cn(
+          "object-cover aspect-5/7 w-auto h-full",
+        )}
         quality={100}
         loading="eager"
       />
       <div className="p-5 grow gap-2 flex items-center justify-evenly">
         <div className="grow-5 shrink-0 basis-0 min-w-auto flex flex-col items-start">
           {/* TODO: Add formatting when rendering title (capitalize words, uppercase roman numerals etc) */}
-          <span className="mb-2 text-xl font-semibold text-center">{ card.title }</span>
+          <a
+            className="mb-2 text-xl font-semibold text-center"
+            href={card.player_url || undefined}
+          >{card.title}</a>
           <span className="text-[#9494a2] flex items-center gap-2.5">
             <Button
               variant={"ghost"}
               className="px-1 text-inherit hover:text-[#5e5e6e]"
               onClick={() => handleWatchedChange(card.id, -1)}
             >
-              <MinusIcon className="size-4.5"/>
+              <MinusIcon className="size-4.5" />
             </Button>
-            {/* TODO: Lower card transparency if watched */}
             <span className="text-xl font-mono tracking-widest">
               {card.episodes_watched}/{card.episodes_total}
             </span>
@@ -75,11 +85,14 @@ export default function CardItem({ card }: Props) {
               className="px-1 text-inherit hover:text-[#5e5e6e]"
               onClick={() => handleWatchedChange(card.id, 1)}
             >
-              <PlusIcon className="size-4.5"/>
+              <PlusIcon className="size-4.5" />
             </Button>
           </span>
         </div>
-        <div className="grow-5 basis-0">
+        <div className={cn(
+          "grow-5 basis-0",
+          { "hidden": isWatched(card) }
+        )}>
           <span className="text-4xl font-mono font-semibold tracking-wide break-keep">
             {countdown ?? '--:--:--:--'}
           </span>
@@ -89,7 +102,7 @@ export default function CardItem({ card }: Props) {
             card={card}
             trigger={
               <Button variant='ghost'>
-                <MoreVerticalIcon className="focus-within:outline-0"/>
+                <MoreVerticalIcon className="focus-within:outline-0" />
               </Button>
             }
           />
